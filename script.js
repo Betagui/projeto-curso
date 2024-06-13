ca027bc5e4a1f362b581709d96ca4525f570fda0
guilherme pereira <166460694+Betagui@users.noreply.github.com>
2024-06-12 23:44:39 -0300

    reajustando botão

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function togglemode() {
    const html = document.documentElement
    html.classList.toggle('light')
-  /// if(html.classList.contains('light')) {
+ const img = document.querySelector("#profile img")
+   if (html.classList.contains('light')){ 
+        img.setAttribute("src", "./imagens/avatar-light.png")
+    } else{ 
+       img.setAttribute("scr", "./imagens/avatar.png")
+    }
+       /// if(html.classList.contains('light')) {
    //   html.classList.remove('light')
    //} else {
      // html.classList.add('light')
    //}
-   const img = document.querySelector("#profile img")
-   if (html.classList.contains('light')){ 
-        img.setAttribute("src", "./imagens/avatar-light.png") 
-    } else{ 
-       img.setAttribute("scr", "./imagens/avatar.png")
-    }   
+   
+       
    
    
 }
