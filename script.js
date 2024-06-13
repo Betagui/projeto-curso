95e48888b06f8f86f96e45f5a0470f9ff2c1a079
guilherme pereira <166460694+Betagui@users.noreply.github.com>
2024-06-12 23:36:41 -0300

    final do projeto

---
+++ script.js
@@ -0,0 +1,17 @@
+function togglemode() {
+   const html = document.documentElement
+   html.classList.toggle('light')
+  /// if(html.classList.contains('light')) {
+   //   html.classList.remove('light')
+   //} else {
+     // html.classList.add('light')
+   //}
+   const img = document.querySelector("#profile img")
+   if (html.classList.contains('light')){ 
+        img.setAttribute("src", "./imagens/avatar-light.png") 
+    } else{ 
+       img.setAttribute("scr", "./imagens/avatar.png")
+    }   
+   
+   
+}
